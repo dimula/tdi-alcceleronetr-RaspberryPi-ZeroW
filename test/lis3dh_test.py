6a087744d718cdc6efc28ee778d2c8f8526705aa
Breiman, Dmitri <dmitri.breiman@worksafebc.com>
2021-06-20 12:13:06 -0700

    polish

--- test/lis3dh_test.py
+++ test/lis3dh_test.py
@@ -7,5 +7,6 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 lis = adafruit_lis331.H3LIS331(i2c, address=0x19)
 lis.range = H3LIS331Range.RANGE_100G
 while True:
-    print("Acceleration : X: %.2f, Y:%.2f, Z:%.2f ms^2" % lis.acceleration)
+    #print("Acceleration : X: %.2f, Y:%.2f, Z:%.2f ms^2" % lis.acceleration)
+    print("Acceleration : X: %.2f, Y:%.2f, Z:%.2f g" % tuple(x/9.8 for x in lis.acceleration))
     time.sleep(1)
